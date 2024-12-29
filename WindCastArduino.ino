fc269db779de58299c8adbbc1b86beb1668951a3
jonjrt <jontech543@protonmail.com>
2024-12-28 20:17:23 -0800

    Establish basic wifi connection

--- WindCastArduino.ino
+++ WindCastArduino.ino
@@ -1,9 +1,26 @@
+#include "apiwifikeys.h" //API keys and wifi credentials
+
+#include <WiFi.h> // Built-in library for ESP32 Wi-Fi
+
 void setup() {
-  // put your setup code here, to run once:
+  // Start the serial monitor for debugging
+  Serial.begin(115200);
+  delay(1000);
 
+  // Connect to Wi-Fi
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  // Establish connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  // Print connection details
+  Serial.println("\nWi-Fi connected!");
+  Serial.print("ESP32 IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+void loop() {}
